179ab622b2ec0e30f15fcd4c2969d37fce9f1e2d
Jude Shaveen <116498182+Shaveenblu@users.noreply.github.com>
2025-02-25 19:20:56 +0530

    Started search function

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Practice from './components/Practice'
+import Search from './Componenets/Search'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [searchTerm, setSearchTerm] = useState('BATMAN')
 
 
   return (
@@ -14,13 +16,14 @@ function App() {
         <div className='wrapper'>
 
           <header>
+            <img src='./assets/hero.png' alt='hero-banner'/>
             <h1>
               Find <span className='text-gradient'>Movies</span> You will enjoy.
             </h1>
           </header>
 
+          <Search searchTerm={searchTerm} setSearchTerm={searchTerm}/>
         </div>
-
     </main>
   )
 }
